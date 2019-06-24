587676f4a1b2263d7377b73dcc7a40ac911b67c3
itohdak <h-ito@jsk.imi.i.u-tokyo.ac.jp>
2019-06-24 17:47:18 +0900

    add dialogflow node for hrp2

---
+++ dialogflow_task_executive/setup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# export GOOGLE_APPLICATION_CREDENTIALS=/etc/opt/jsk/robot/dialogflow-hrp2.json
+export GOOGLE_APPLICATION_CREDENTIALS=/etc/opt/jsk/robot/JSK-HRP2-16a6aaf1474e.json
+export DIALOGFLOW_PROJECT_ID=hrp2-xhwxsc
+echo $GOOGLE_APPLICATION_CREDENTIALS
+echo $DIALOGFLOW_PROJECT_ID
