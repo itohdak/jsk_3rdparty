ce2aa63182516465bbf6b8dc6b49db2f890727f4
Hideaki Ito <h-ito@jsk.imi.i.u-tokyo.ac.jp>
2019-10-25 13:45:44 +0900

    [dialogflow_task_executive] minor change for hrp2 demo

--- dialogflow_task_executive/setup.sh
+++ dialogflow_task_executive/setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-source /home/leus/app_manager_ws/devel/setup.bash # app manager
+# source /home/leus/app_manager_ws/devel/setup.bash # app manager
 
 export GOOGLE_APPLICATION_CREDENTIALS=/etc/opt/jsk/robot/JSK-HRP2-16a6aaf1474e.json
 export DIALOGFLOW_PROJECT_ID=hrp2-xhwxsc
